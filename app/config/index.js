f31ba7df1ce36a8eadb746ff2b769b3de1138374
larry eliemenye <denachural@gmail.com>
2017-11-19 17:24:42 +0000

    refactor parse config code

--- app/config/index.js
+++ app/config/index.js
@@ -2,22 +2,29 @@ const _ = require('lodash');
 const config = require('./config.json');
 
 /**
+ * formats placeholder and returns final string
+ * @param {*} value 
+ */
+const formatKey = function (value) {
+    const formattedKey = _.chain(value).replace('@', '').snakeCase().toUpper().value();
+    if (!process.env[formattedKey]) {
+        throw new Error (`ENV VAR "${formattedKey}" NOT SET`);
+    }
+    return process.env[formattedKey];
+}
+
+/**
  * parse config object and replace placeholder with values from env var
  * @param {*} configObject 
  */
 function parseConfig(configObject) {
     return _.mapValues(configObject, (value, key) => {
         if (_.isString(value) && value.startsWith('@')) {
-            const formattedKey = _.chain(value)
-                .replace('@', '')
-                .snakeCase()
-                .toUpper()
-                .value();
+            return formatKey(value);
+        }
 
-            if (!process.env[formattedKey]) {
-                throw new Error (`ENV VAR "${formattedKey}" NOT SET`);
-            }
-            return process.env[formattedKey];
+        if(_.isObject(value)) {
+            return parseConfig(value);
         }
         return value;
     });
