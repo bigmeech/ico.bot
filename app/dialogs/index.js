548460fa5509316f0380508446e207597d06a3bb
larry eliemenye <denachural@gmail.com>
2017-11-19 20:38:26 +0000

    Added some help content

--- app/dialogs/index.js
+++ app/dialogs/index.js
@@ -1,27 +1,39 @@
 exports.info = function (session){
-    session.send('Vendex ICO info goes here');
+    session.endDialog('Vendex ICO info goes here');
 }
 
 exports.request = function (session) {
-    session.send('Buy some tokens. Min = 1500 VEND');
+    session.endDialog('Buy some tokens. Min = 1500 VEND');
 }
 
 exports.rules = function (session) {
-    session.send('Vendex ICO Rules go here');
+    session.endDialog('Vendex ICO Rules go here');
 }
 
 exports.create = function (session) {
-    session.send('Create address for user to send VEND token');
+    session.endDialog('Create address for user to send VEND token');
 }
 
 exports.balance = function (session) {
-    session.send('Checks balance of address created by US');
+    session.endDialog('Checks balance of address created by US');
 }
 
 exports.register = function (session) {
-    session.send('Register user for our ICO (Some users might want to start anonymous)')
+    session.endDialog('Register user for our ICO (Some users might want to start anonymous)')
 }
 
 exports.delete = function (session) {
-    session.send('Delete Account: Make sure you dont have funds in it');
+    session.endDialog('Delete Account: Make sure you dont have funds in it');
+}
+
+exports.help = function (session) {
+    session.endDialog('### Command Options '
+        + '\n -----------------------------------------------------------------------'
+        + '\n - ```/info```&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Displays some information for our token sales '
+        + '\n - ```/register```&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Help us remember you' 
+        + '\n - ```/buy [units]```&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Returns an address with the '
+        + '\n - ```/transfer [from] [to]```&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - tranfer token to another address'
+        + '\n - ```/balance [addr] [to]```&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - return balance of address'
+        + '\n - ```/help [addr] [to]```&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - return balance of address'
+    );
 }
--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ const restify = require('restify');
 const config = require('./config')
 const logger = require('simple-node-logger').createSimpleLogger();
 
+const server = restify.createServer();
 let SERVICE_PORT = config.service.port || 3000;
 
 const dialogs = require('./dialogs');
@@ -13,15 +14,38 @@ const connector = new Builder.ChatConnector({
 
 const bot = new Builder.UniversalBot(connector);
 
-bot.dialog('/info', dialogs.info);
-bot.dialog('/rules', dialogs.rules);
-bot.dialog('/create', dialogs.create);
-bot.dialog('/delete', dialogs.delete);
-bot.dialog('/request', dialogs.request);
-bot.dialog('/register', dialogs.register);
-bot.dialog('/balance', dialogs.balance);
+bot.dialog('/info', dialogs.info.bind(bot)).triggerAction({
+    matches: /^info$/i,
+});
 
-const server = restify.createServer();
+bot.dialog('/rules', dialogs.rules).triggerAction({ 
+    matches : /^rules$/i
+});
+
+bot.dialog('/create', dialogs.create).triggerAction({
+    matches: /^create$/i
+});
+
+bot.dialog('/delete', dialogs.delete).triggerAction({
+    matches: /^delete$/i
+});
+
+bot.dialog('/request', dialogs.request).triggerAction({
+    matches: /^request$/i
+});
+
+bot.dialog('/register', dialogs.register).triggerAction({
+    matches: /^register$/i
+});
+
+bot.dialog('/balance', dialogs.balance).triggerAction({
+    matches: /^balance$/i
+});
+
+bot.dialog('/help', dialogs.help).triggerAction({
+    matches: /^help$/i
+});
+
 server.post('/api/messages', connector.listen());
 
 server.listen(SERVICE_PORT, () => {
